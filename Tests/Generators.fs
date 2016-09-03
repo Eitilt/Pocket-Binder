fe8645cf43bf4be4727179a61f79b20327305646
Sam May <ag.eitilt@gmail.com>
2016-09-02 20:09:53 -0700

    Added (rough) generation of attributes

--- Tests/Generators.fs
+++ Tests/Generators.fs
@@ -14,10 +14,8 @@ open System.Xml.Linq
 // Based on <https://gist.github.com/mavnn/5976004> and the associated post at
 // <http://blog.mavnn.co.uk/fscheck-breaking-your-code-in-new-and-exciting-ways/>
 module Xml =
-    type ElementName = ElementName of string with
-        static member op_Explicit (ElementName s) = s
-    type AttributeName = AttributeName of string with
-        static member op_Explicit (AttributeName s) = s
+    type Name = Name of string with
+        static member op_Explicit (Name s) = s
 
     type AttributeValue = AttributeValue of string with
         static member op_Explicit (AttributeValue s) = s
@@ -29,43 +27,59 @@ module Xml =
 
     type Tree =
     | Leaf of string
-    | Branch of string * Tree list
+    | Element of string * (string * string) list * Tree list
 
 
-    let genTreeNames names =
-        //TODO: Implement generation of attributes and contained text
-        let rec generate size names =
+    let genTreeNamesAttrs names attrs =
+
+        let rec generate names size =
             match size with
             | 0            -> Gen.map Leaf (Gen.elements names)
             | n when n > 0 ->
+                //TODO: Poor randomness within each test, but decent enough over multiple
+                let sampleAttr =
+                    match Gen.subListOf attrs
+                            |> Gen.sample 3 1 with
+                    // Should only have generated one sublist, but pattern match anyway
+                    | []          -> []
+                    | (head :: _) -> head
+
                 Gen.oneof [
                     // Allow for jagged trees by skipping ahead an iteration
-                    generate (n / 2) names
+                    generate names (n / 2)
 
                     // Generate nested trees of decreasing size...
                     Gen.sized <| fun s ->
-                        Gen.listOf (generate (n / 2) names)
+                        Gen.listOf (generate names (n / 2))
                         |> Gen.resize (s |> float |> sqrt |> int)
                     // ...and encapsulate them in a randomly-named tag
-                    |> Gen.map2 (fun name contents -> Branch (name, contents)) (Gen.elements names)
+                    |> Gen.map2 (fun name contents -> Element (name, sampleAttr, contents)) (Gen.elements names)
                 ]
             | _            -> invalidArg "size" "Size most be positive."
 
-        Gen.sized <| fun size ->
-            generate size names
+        Gen.sized <| generate names
+
+    let genTreeNames (names : string list) =
+        Gen.listOfLength names.Length Arb.generate<AttributeValue>
+        |> Gen.eval names.Length (Random.newSeed ())
+        |> List.map2 (fun n (AttributeValue v) -> n, v) names
+        |> genTreeNamesAttrs names
 
     let genTree =
         Gen.sized <| fun size ->
-            Gen.nonEmptyListOf Arb.generate<ElementName>
+            Gen.nonEmptyListOf Arb.generate<Name>
             // The size calculation allows more repetition in the names while
-            // not running into issues with a 0 size
+            //   not running into issues with a 0 size
+            // Unfortunately, the required seed depends purely on the time
+            //   when the test is run, due to a lack of support for extracting
+            //   random values from a generator; this may be fixed in FsCheck 3
             |> Gen.eval ((size / 2) + 1) (Random.newSeed ())
-            |> List.map (fun (ElementName n) -> n)
+            |> List.map (fun (Name n) -> n)
             |> genTreeNames
 
 
     let genDoc tree =
-        // Precompose a couple functions shared between branches
+        // Precompose a couple functions used multiple times
         let newElem = XName.Get >> XElement
         let newDoc  = newElem >> XDocument
 
@@ -73,8 +87,10 @@ module Xml =
             // Transcribe the `Tree` union into nested `XElement`s
             let fromChild child =
                 match child with
-                | Leaf   name            -> newElem name
-                | Branch (name, subtree) -> generate (newElem name) subtree
+                | Leaf    name                  -> newElem name
+                | Element (name, atts, subtree) -> let elem : XElement = generate (newElem name) subtree
+                                                   List.map (fun (a, v) -> elem.SetAttributeValue (XName.Get a, v)) atts |> ignore
+                                                   elem
 
             // Apply the above to the current node's children
             current.Add (List.map fromChild children |> List.toArray)
@@ -82,8 +98,8 @@ module Xml =
 
         // Start the recursion, or use the simpler form if that's what's passed
         match tree with
-        | Leaf   name            -> newDoc name
-        | Branch (name, subtree) ->
+        | Leaf   name                   -> newDoc name
+        | Element (name, atts, subtree) ->
             let doc = newDoc name
             generate doc.Root subtree |> ignore
             doc
@@ -93,7 +109,7 @@ module Xml =
         match tree with
         // If the node's a leaf, delete it
         | Leaf _                  -> Seq.empty
-        | Branch (name, children) ->
+        | Element (name, atts, children) ->
             if children.Length <= 1
                 // If it only contains a single element, turn it into a leaf node
                 then Seq.singleton <| Leaf name
@@ -103,10 +119,10 @@ module Xml =
                          // If `c` was a leaf before shrinking, remove it from the list
                          // While we could match against `c` directly, this is slightly
                          //   less dependent on the exact format of the other returns
-                         | e when Seq.isEmpty e -> Branch (name, Seq.toList (Seq.append (Seq.take i children) (Seq.skip (i + 1) children)))
+                         | e when Seq.isEmpty e -> Element (name, atts, Seq.toList (Seq.append (Seq.take i children) (Seq.skip (i + 1) children)))
                          // Otherwise the value returned from `shrinkTree` is already
                          // all children with `c` having been shrunk properly
-                         | shrunk               -> Branch (name, Seq.toList shrunk)
+                         | shrunk               -> Element (name, atts, Seq.toList shrunk)
                      ) children
 
 
@@ -117,31 +133,30 @@ module Xml =
         // Define a lookahead to prevent newlines at the end of the string
         static let regEnd  = "(?![\r\n])$"
 
-        static let arbNonNull =
+        static let arbNonNullString =
             Arb.from<string>
             // Adding an additional filter is easier than trying to rework for NonNull<string>
             |> Arb.filter (fun s -> not <| System.Object.ReferenceEquals (s, null))
-        static let arbName =
-            arbNonNull
+
+        static member Name () =
+            arbNonNullString
             |> Arb.filter (fun s -> Regex.IsMatch (s, "^" + regName + regEnd))
             |> Arb.filter (fun s -> not <| s.ToUpper().StartsWith "XML")
-
-        static member ElementName () =
-            arbName |> Arb.convert ElementName string
-        static member AttributeName () =
-            arbName |> Arb.convert AttributeName string
+            |> Arb.convert Name string
 
         static member AttributeValue () =
-            arbNonNull
+            arbNonNullString
             |> Arb.filter (fun s -> Regex.IsMatch (s, "^([^<&\"]|" + regRef + ")*" + regEnd))
+            |> Arb.filter (fun s -> not <| s.ToUpper().StartsWith "XML")
+            |> Arb.filter (fun s -> not <| Regex.IsMatch (s, "\W"))
             |> Arb.convert AttributeValue string
         static member TextValue () =
-            arbNonNull
+            arbNonNullString
             // Because even entity references are counted as a separate type of node, this is simple
             |> Arb.filter (fun s -> Regex.IsMatch (s, "^[^<&]*" + regEnd))
             |> Arb.convert TextValue string
         static member ReferenceValue () =
-            arbNonNull
+            arbNonNullString
             |> Arb.filter (fun s -> Regex.IsMatch (s, "^" + regRef + regEnd))
             |> Arb.convert ReferenceValue string
 
